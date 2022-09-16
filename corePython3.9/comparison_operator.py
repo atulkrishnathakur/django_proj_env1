06ac49d3b8104389a0413190a5e3826ce8581484
Atul Krishna Thakur <gitvcs@gmail.com>
2022-09-16 11:02:31 +0530

    logical operator

---
+++ corePython3.9/comparison_operator.py
@@ -0,0 +1,37 @@
+# == (equal)
+x = 5
+y = "5"
+
+print(x == y)
+
+
+# !=(Not equal)
+x = 5
+y = 3
+
+print(x != y)
+
+# >(greater than)
+x = 5
+y = 3
+
+print(x > y)
+
+# <(less than)
+x = 5
+y = 3
+
+print(x < y)
+
+# >=(Greater than or equal to)
+x = 5
+y = 3
+
+print(x >= y)
+
+# <=(Less than or equal to)
+x = 5
+y = 3
+
+print(x <= y)
+
